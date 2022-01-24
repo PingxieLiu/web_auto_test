f63fdd2d30bf19dda2078460e18322388605e324
“lhk” <15377019350@163.com>
2022-01-24 22:10:38 +0800

    web自动化第一次提交代码

---
+++ src/test/java/test.java
@@ -0,0 +1,12 @@
+/**
+ * @Classname test
+ * @Description TODO
+ * @Date 1/24/22 10:07 PM
+ * @Created by lhk
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+    }
+}
